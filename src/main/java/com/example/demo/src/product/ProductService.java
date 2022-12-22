62515b9cb7aabff22f109ce2bdd518eecdb44e0d
tanonkim <robbins4bos@gmail.com>
2022-12-23 01:40:53 +0900

    feat : jwt

--- src/main/java/com/example/demo/src/product/ProductService.java
+++ src/main/java/com/example/demo/src/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.demo.src.product;
 
 import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
 import com.example.demo.config.BaseResponseStatus;
 import com.example.demo.src.product.model.PostProductReq;
 import com.example.demo.src.product.model.PostProductRes;
@@ -9,20 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@Transactional
 public class ProductService {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final ProductDao productDao;
-//    private final ProductProvider productProvider;
     private final JwtService jwtService;
 
     @Autowired
     public ProductService(ProductDao productDao, JwtService jwtService) {
         this.productDao = productDao;
-//        this.productProvider = productProvider;
         this.jwtService = jwtService;
     }
 
@@ -30,6 +31,11 @@ public class ProductService {
     public PostProductRes createPost(PostProductReq postProductReq) throws BaseException{
         if(postProductReq.getProductName() == null){
             throw new BaseException(BaseResponseStatus.POST_PRODUCT_EXISTS_NAME);
+        }
+        int userIdx = postProductReq.getUserIdx();
+        int userIdxByJwt = jwtService.getUserIdx();
+        if(userIdx != userIdxByJwt){
+            throw new BaseException(BaseResponseStatus.INVALID_USER_JWT);
         }
         try{
             int productIdx = productDao.createPost(postProductReq);
