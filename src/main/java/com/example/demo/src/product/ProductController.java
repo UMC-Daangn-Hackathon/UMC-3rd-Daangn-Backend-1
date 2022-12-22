0708d1c13c1565ce2e057692e2666dad037dfd0a
민병욱 <woook27@naver.com>
2022-12-22 23:20:00 +0900

    feat: getProducts

--- src/main/java/com/example/demo/src/product/ProductController.java
+++ src/main/java/com/example/demo/src/product/ProductController.java
@@ -2,8 +2,7 @@ package com.example.demo.src.product;
 
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
-import com.example.demo.src.user.UserProvider;
-import com.example.demo.src.user.UserService;
+import com.example.demo.src.product.model.GetProductRes;
 import com.example.demo.src.user.model.*;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
@@ -17,8 +16,37 @@ import static com.example.demo.config.BaseResponseStatus.*;
 import static com.example.demo.config.BaseResponseStatus.INVALID_USER_JWT;
 import static com.example.demo.utils.ValidationRegex.isRegexEmail;
 
+@RestController
+@RequestMapping("/app/products")
 public class ProductController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private final ProductProvider productProvider;
+    @Autowired
+    private final ProductService productService;
+
+    public ProductController(ProductProvider productProvider, ProductService productService){
+        this.productProvider = productProvider;
+        this.productService = productService;
+    }
+
+    /**
+     * 상품 조회 API
+     * [GET] /products? addressDetail=
+     * @return BaseResponse<List<GetProductRes>>
+     */
+    //Query String
+    @ResponseBody
+    @GetMapping("/{productAddress}") // (GET) 127.0.0.1:9000/app/products
+    public BaseResponse<List<GetProductRes>> getProducts(@PathVariable("productAddress") String productAddress) {
+        try{
+            // Get Products
+            List<GetProductRes> getProductsRes = productProvider.getProducts(productAddress);
+            return new BaseResponse<>(getProductsRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
 }
 
